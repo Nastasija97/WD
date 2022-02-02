11ea4d1a11438d732dd1eae46ad9728ce075a6f9
nastasijaperovic <nastasija.perovic.13.20@ict.edu.rs>
2022-02-02 17:09:57 +0100

    first commit

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 9, 2021 23:00:00").getTime();
+const menuBtn=document.querySelector('.menu-btn');
+const navLinks=document.querySelector('.nav-links');
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+menuBtn.addEventListener('click',()=>{
+    navLinks.classList.toggle('mobile-menu')
+})
 
-  // Get todays date and time
-  var now = new Date().getTime();
+var btnContainer=document.getElementById("nav-links");
+var buttons=btnContainer.getElementsByClassName("nav-btn");
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function(){
+        var current=document.getElementsByClassName("active");
+        current[0].className=current[0].className.replace("active","");
+        this.className+=" active";
+    });
+}
