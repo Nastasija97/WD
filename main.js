309d49aa02519cfeea5c5eef3340f97acb64465d
nastasijaperovic <nastasija.perovic.13.20@ict.edu.rs>
2022-02-04 13:18:20 +0100

    final submit i hope

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const menuBtn=document.querySelector('.menu-btn');
-const navLinks=document.querySelector('.nav-links');
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener('click',()=>{
-    navLinks.classList.toggle('mobile-menu')
-})
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
 
-var btnContainer=document.querySelector("#nav-links");
-var buttons=btnContainer.getElementsByClassName("nav-btn");
+// var btnContainer=document.querySelector("#nav-links");
+// var buttons=btnContainer.getElementsByClassName("nav-btn");
 
 // for(var i=0;i<buttons.length;i++){
 //     buttons[i].addEventListener("click",function(){
